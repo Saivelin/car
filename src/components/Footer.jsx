7f3b41d720290f49759870210e37c0c98395061c
Saivelin <vlad.pochkalov@mail.ru>
2023-04-28 15:20:19 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,36 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer>
-            footer
+        <footer className='footer'>
+            <div className="footer__main">
+                <div className="footer__main-item">
+                    <h4>Сервисы</h4>
+                    <p>Подбор авто</p>
+                    <p>Оценки рисков</p>
+                    <p>Поиск подборщиков</p>
+                    <p>Подбор запчестей</p>
+                    <p>Форум CarTron</p>
+                    <p>Страховка / Кредитование</p>
+                </div>
+                <div className="footer__main-item">
+                    <h4>О компании CarTron</h4>
+                    <p>Руководство CarTron</p>
+                    <p>Карьера в CarTron</p>
+                    <p>Гарантии</p>
+                    <p>Инвесторы</p>
+                    <p>Этика и политика компании</p>
+                    <p>Контактная информация</p>
+                </div>
+                <div className="footer__main-item">
+                    <h4>Пользователям</h4>
+                    <p>Правила площадки</p>
+                    <p>Lorem ipsum</p>
+                    <p>Lorem ipsum</p>
+                    <p>Lorem, ipsum dolor</p>
+                    <p>Lorem ipsum dolor sit</p>
+                    <p>Lorem, ipsum dolor</p>
+                </div>
+            </div>
         </footer>
     );
 };
