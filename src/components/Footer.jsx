fd70c94c315e9e9a79d738ff41515d0a9c1d1b56
Saivelin <vlad.pochkalov@mail.ru>
2023-04-28 16:14:28 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -32,6 +32,22 @@ const Footer = () => {
                     <p>Lorem, ipsum dolor</p>
                 </div>
             </div>
+            <div className="footer__footer">
+                <div className="footer__footer-itemWrapper">
+                    <span className='footer__footer-item'>Политика конфиденциальности</span>
+                </div>
+                <div className="footer__footer-itemWrapper">
+                    <span className='footer__footer-item'>Использование cookies</span>
+                </div>
+                <div className="footer__footer-itemWrapper">
+                    <span className='footer__footer-item'>Юридическая Информация</span>
+                </div>
+                <div className="footer__footer-itemWrapper footer__footer-society">
+                    <img src="/classmates.png" alt="Мы в однокласниках" className='footer__footer-classmates' />
+                    <img src="/vk.png" alt="Мы ВКонтакте" />
+                    <img src="/youtube.png" alt="Мы на YouTube" />
+                </div>
+            </div>
         </footer>
     );
 };
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Footer from './Footer';
 import Header from './Header/Header';
+import Head from 'next/head';
 
 const Layout = ({ children }) => {
     return (
         <div className='page'>
+            <Head>
+                <title>Car-tron</title>
+                <link rel="icon" type="image/x-icon" href="/logo.ico" />
+            </Head>
             <Header />
             {children}
             <Footer />
