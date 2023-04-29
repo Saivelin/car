0b361c197cdfd5e579ee93030620b9989c98fa38
Saivelin <vlad.pochkalov@mail.ru>
2023-04-29 21:58:01 +0300

    userPage .8

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,16 +8,16 @@ const Header = () => {
         <header className="header">
             <nav className="header__nav">
                 <div className="header__nav-item header__nav-lang">RU</div>
-                <div className="header__nav-item"><Link href={"/"} className={pathname == "/" ? "header__nav-active" : ""}>Объявления</Link></div>
-                <div className="header__nav-item"><Link href={"/details"} className={pathname == "/details" ? "header__nav-active" : ""}>Запчасти</Link></div>
+                <div className="header__nav-item"><Link href={"/"} className={pathname == "/" ? "header__nav-active" : "header__nav-hover"}>Объявления</Link></div>
+                <div className="header__nav-item"><Link href={"/details"} className={pathname == "/details" ? "header__nav-active" : "header__nav-hover"}>Запчасти</Link></div>
                 <div className="header__nav-item header__nav-logo">
                     <Link href={"/"} className="">
                         <img src="/logo.webp" alt="car-tron" />
                     </Link>
                 </div>
-                <div className="header__nav-item"><Link href={"/dilers"} className={pathname == "/dilers" ? "header__nav-active" : ""}>Диллеры</Link></div>
-                <div className="header__nav-item"><Link href={"/services"} className={pathname == "/services" ? "header__nav-active" : ""}>Услуги</Link></div>
-                <div className="header__nav-item header__nav-personal">
+                <div className="header__nav-item"><Link href={"/dilers"} className={pathname == "/dilers" ? "header__nav-active" : "header__nav-hover"}>Диллеры</Link></div>
+                <div className="header__nav-item"><Link href={"/services"} className={pathname == "/services" ? "header__nav-active" : "header__nav-hover"}>Услуги</Link></div>
+                <div className="header__nav-item header__nav-personal header__nav-hover">
                     <Link href={"/users/1"} className={pathname.includes("/users") ? "header__nav-active" : ""}>
                         <img src="/personal.webp" alt="Личный кабинет" />
                     </Link>
