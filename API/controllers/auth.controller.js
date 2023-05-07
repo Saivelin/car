eaf197a23a2ed8544ed0ee119563b21d4fc3e8de
Saivelin <vlad.pochkalov@mail.ru>
2023-05-07 21:10:25 +0300

    API login + registration

--- API/controllers/auth.controller.js
+++ API/controllers/auth.controller.js
@@ -1,17 +1,59 @@
+const db = require('../db')
+const bcrypt = require("bcryptjs")
+const { validationResult } = require('express-validator')
+const jwt = require('jsonwebtoken')
+const { secret } = require('../config/auth.config.js')
+
+const generateAccessToken = (id, role, nsp) => {
+    const payload = {
+        id,
+        role,
+        nsp
+    }
+    return jwt.sign(payload, secret, { expiresIn: "24h" })
+}
+
 class authController {
     async registration(req, res) {
         try {
-
+            const errors = validationResult(req)
+            console.log(errors)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ status: false, message: errors.errors })
+            }
+            const { nsp, password } = req.body
+            const candidate = await db.query(`SELECT * FROM users WHERE nsp=$1`, [nsp])
+            if (candidate.rows[0]) {
+                console.log("Такой пользователь уже существует")
+                return res.status(400).json({ status: false, message: "This user already exist" })
+            }
+            console.log("Такого пользователя пока нет")
+            const hashPassword = bcrypt.hashSync(password, 8)
+            console.log(hashPassword)
+            const newPerson = await db.query(`INSERT INTO users (nsp, password, role) values ($1, $2, $3) RETURNING *`, [nsp, hashPassword, "user"])
+            return res.status(200).json({ status: true, user: newPerson.rows[0] })
         } catch (e) {
-            res.json({ status: false, errorName: e })
+            res.status(400).json({ status: false, errorName: e })
         }
     }
 
     async login(req, res) {
         try {
-
+            const { nsp, password } = req.body
+            const user = await db.query(`SELECT * FROM users WHERE nsp=$1`, [nsp])
+            if (!user.rows[0]) {
+                return res.status(404).json({ status: false, message: "Такого пользователя не существует" })
+            }
+            const validPassword = bcrypt.compareSync(password, user.rows[0].password)
+            if (validPassword === true) {
+                const token = generateAccessToken(user.rows[0].id, user.rows[0].role, user.rows[0].nsp)
+                return res.json({ token: token })
+            }
+            else {
+                return res.status(400).json({ status: false, message: "Неверный пароль" })
+            }
         } catch (e) {
-            res.json({ status: false, errorName: e })
+            res.status(400).json({ status: false, errorName: e })
         }
     }
 
@@ -19,7 +61,7 @@ class authController {
         try {
             res.json("server worked");
         } catch (e) {
-            res.json({ status: false, errorName: e })
+            res.status(400).json({ status: false, errorName: e })
         }
     }
 }
--- API/routes/authRouter.routes.js
+++ API/routes/authRouter.routes.js
@@ -1,9 +1,17 @@
 const Router = require('express')
 const router = new Router()
 const controller = require('../controllers/auth.controller')
+const { check } = require("express-validator")
+const authMiddleware = require('../middleware/authMiddleware')
+const roleMiddlewere = require('../middleware/roleMiddleware')
 
-router.post('/registration', controller.registration)
+router.post('/registration',
+    [
+        check('nsp', "Имя пользователя не может быть пустым").notEmpty(),
+        check('password', "Пароль должен быть больше 4 и меньше 30 символов").isLength({ min: 4, max: 30 })
+    ]
+    , controller.registration)
 router.post('/login', controller.login)
-router.get('/getUsers', controller.getUsers)
+router.get('/getUsers', roleMiddlewere(['user']), controller.getUsers)
 
 module.exports = router
