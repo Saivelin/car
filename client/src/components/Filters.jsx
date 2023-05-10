51688267fc679303d5fcbe3a83c818c23ba5a972
Saivelin <vlad.pochkalov@mail.ru>
2023-05-10 19:01:43 +0300

    authtorization

--- client/src/components/Filters.jsx
+++ client/src/components/Filters.jsx
@@ -1,17 +1,29 @@
 import { useState } from 'react';
-import FilterSelect from './UI/FilterSelect';
+import NewAdSelect from './UI/NewAdSelect';
 import FilterBtn from './UI/FilterBtn';
+import FilterSelect from './UI/FilterSelect';
+import InputPrimary from './UI/InputPrimary';
 
 const Filters = () => {
     const [count, setCount] = useState(318);
 
     return (
         <div className='filter'>
-            <FilterSelect placeholder={"Марка"} />
-            <FilterSelect placeholder={"Модель"} />
-            <FilterSelect placeholder={"Коробка"} />
-            <FilterSelect placeholder={"Двигатель"} />
-            <FilterSelect placeholder={"Привод"} />
+            <NewAdSelect placeholder={"Марка"} />
+            <NewAdSelect placeholder={"Модель"} />
+            <NewAdSelect placeholder={"Поколение"} />
+            <NewAdSelect placeholder={"Кузов"} />
+            <NewAdSelect placeholder={"Коробка"} />
+            <NewAdSelect placeholder={"Двигатель"} />
+            <NewAdSelect placeholder={"Привод"} />
+            <div className="filter__twoInOne">
+                <InputPrimary classes="input-filter" placeholder="Цена от" />
+                <InputPrimary classes="input-filter" placeholder="Цена до" />
+            </div>
+            <div className="filter__twoInOne">
+                <InputPrimary classes="input-filter" placeholder="Объем от" />
+                <InputPrimary classes="input-filter" placeholder="Объем до" />
+            </div>
             <FilterBtn>
                 Показать {count} предложений
             </FilterBtn>
